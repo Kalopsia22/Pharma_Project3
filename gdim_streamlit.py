03afd7f200f5507e545a98ebc272ba0749b540e9
Atharva Rewatkar <94364719+Kalopsia22@users.noreply.github.com>
2026-03-12 21:26:47 +0530

    Update gdim_streamlit.py

--- gdim_streamlit.py
+++ gdim_streamlit.py
@@ -19,7 +19,7 @@ import time
 # ─────────────────────────────────────────────
 st.set_page_config(
     page_title="Global Drug Intelligence Monitor",
-    page_icon="⊕",
+    page_icon="⚕️",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
